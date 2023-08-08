53cd50a1c534e865c4d8eb1f4c0a8fc88353eb06
zamanmohamd <Zamanmohamed97@gmail.com>
2023-08-08 23:12:28 +0530

    add

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,10 @@
-//Middleware වලට (req, res, next)  යන 3ම පවති
-//if route we enter wrong route --> eg:- http://localhost:5000/asdffgggddd
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
-  //pass error to next middleware
   next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-  //statusCode එක 200 තිබුනා  නම් 500 ට මාරු කරනවා
-  //statusCode එක 200 නොතිබුනා නම් එහි අගය වෙනස් කරන්නෙ නැත
-
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
